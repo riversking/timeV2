1c54b60ac3abcf976fd09fcb242fb28b6dd9c641
riversking <kingjames6236@163.com>
2025-11-29 08:29:11 +0800

    feat: upgrade dependencies and refactor tasklet execution

--- timer-batch/src/main/java/com/rivers/batch/task/BusinessTasklet.java
+++ timer-batch/src/main/java/com/rivers/batch/task/BusinessTasklet.java
@@ -1,7 +1,10 @@
 package com.rivers.batch.task;
 
+import com.rivers.core.client.DynamicServiceClient;
+import com.rivers.core.entity.JobParamReq;
 import com.rivers.core.feign.DynamicClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -17,21 +20,27 @@ import java.util.Map;
 public class BusinessTasklet implements Tasklet {
 
 
-    private final DynamicClient dynamicClient;
+    private final DynamicServiceClient dynamicServiceClient;
 
-
-    public BusinessTasklet(DynamicClient dynamicClient) {
-        this.dynamicClient = dynamicClient;
+    public BusinessTasklet(DynamicServiceClient dynamicServiceClient) {
+        this.dynamicServiceClient = dynamicServiceClient;
     }
+
 
     @Override
     public RepeatStatus execute(@NonNull StepContribution contribution, @NonNull ChunkContext chunkContext) {
         try {
-            Map<String, Object> jobParameters = chunkContext.getStepContext().getJobParameters();
-            log.info("Job parameters: {}", jobParameters);
-            String taskName = (String) jobParameters.get("taskName");
+            JobParameters jobParameters = contribution.getStepExecution().getJobParameters();
+            String taskName = jobParameters.getString("taskName");
+            String serviceName = jobParameters.getString("serviceName");
             log.info("Executing business task for job: {}", taskName);
-//            String result = dynamicClient.executePostApi(jobName, "/api/business/execute", jobParameters);
+            log.info("Executing business task for service: {}", serviceName);
+            Map<String, Object> parameters = chunkContext.getStepContext().getJobParameters();
+            JobParamReq jobParamReq = new JobParamReq();
+            jobParamReq.setTaskName(taskName);
+            jobParamReq.setParams(parameters);
+            dynamicServiceClient.post(serviceName, "/job/execute", jobParamReq, String.class)
+                    .subscribe(i -> log.info("Business task executed successfully: {}", i));
             ExecutionContext executionContext = contribution.getStepExecution().getExecutionContext();
             executionContext.put("jobParameters", jobParameters);
             log.info("Execution context: {}", executionContext);
