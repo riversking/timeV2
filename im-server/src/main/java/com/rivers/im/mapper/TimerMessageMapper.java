8703989132c6aead74d678cf2a3aba7d415f33e8
riversking <kingjames6236@163.com>
2026-05-01 08:55:30 +0800

    feat(im): 添加定时消息 Mapper 接口
    - 新增 TimerMessageMapper 接口，用于定时消息的 CRUD 操作
    - 该接口继承自 ReactiveCrudRepository，支持响应式操作
    - 定义了 TimerMessage 实体类的仓库操作，使用 Long 类型的主键

--- im-server/src/main/java/com/rivers/im/mapper/TimerMessageMapper.java
+++ im-server/src/main/java/com/rivers/im/mapper/TimerMessageMapper.java
@@ -1,6 +1,7 @@
 package com.rivers.im.mapper;
 
+import com.rivers.im.entity.TimerMessage;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface TimerMessageMapper extends ReactiveCrudRepository<TimerMessageMapper, Long> {
+public interface TimerMessageMapper extends ReactiveCrudRepository<TimerMessage, Long> {
 }
